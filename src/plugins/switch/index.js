b113e617e63c527b2138e0c45ce776448e04005f
Cameron Little <cameron@camlittle.com>
2017-02-19 20:36:37 -0800

    Added new switch accessory! Probably a living room light

---
+++ src/plugins/switch/index.js
@@ -0,0 +1,59 @@
+import fetch from 'node-fetch';
+import { Characteristic, Service } from 'hap-nodejs';
+
+import HomebridgeAccessory from '../homebridgeAccessory';
+import pkg from './package.json';
+import globalInfo from '../../globalInfo';
+import DeviceManager from '../deviceManager';
+
+
+export class Switch extends DeviceManager {
+    // reverse -- I programed the "open" circuit logic backwards in my switch,
+    // this allows me to fix it homebridge side so I don't have to break open
+    // the box to flash new firmware.
+    constructor(server, log, reverse = false) {
+        super(server, log);
+
+        this.reverse = reverse;
+    }
+
+    set(on) {
+        if (this.reverse) {
+            on = !on; // eslint-disable-line no-param-reassign
+        }
+        return fetch(`${this.server}/?open=${on ? 'false' : 'true'}`, {
+            method: 'POST',
+            ...this.defaultFetchOptions,
+        })
+        .then(r => r.ok)
+        .then(r => {
+            console.log(r);
+            return r;
+        });
+    }
+}
+
+export default class SwitchAccessory extends HomebridgeAccessory {
+    constructor(log, config) {
+        super(log, config);
+
+        const obj = new Switch('http://10.0.1.6', log, true);
+
+        const info = new Service.AccessoryInformation();
+        info.setCharacteristic(Characteristic.Name, this.name);
+        info.setCharacteristic(Characteristic.Manufacturer, globalInfo.Manufacturer);
+        info.setCharacteristic(Characteristic.Model, pkg.name);
+        info.setCharacteristic(Characteristic.SoftwareRevision, pkg.version);
+
+        const switchService = new Service.Switch();
+        switchService
+            .getCharacteristic(Characteristic.On)
+            .on('get', this.doGet(() => obj.getStatus()))
+            .on('set', this.doSet(value => obj.set(value)));
+
+        this.services = [
+            switchService,
+        ];
+    }
+}
+
